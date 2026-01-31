ad271d22376168a7e17fccd35cbee3e604e7df32
On-cloud7 <priyanka.p62003@gmail.com>
2026-01-31 22:21:10 +0530

    project added

---
+++ project-1.py
@@ -0,0 +1,12 @@
+#input from the user
+#total rent
+# total food ordered for snacking
+# charge per unit
+
+#output
+#total amount you've to pay is
+
+rent = int(input("Enter the flat rent:"))
+food = int (input("enter the total food ordered ="))
+electricity_spent = int(input("enter the total of electricity spend:"))
+charge_per_unit = int(input("enter the the charge per unit:"))
