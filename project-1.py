a54c9d892fff9f5651cb47c3df3046b826754791
On-cloud7 <priyanka.p62003@gmail.com>
2026-01-31 22:25:39 +0530

    changes made

--- project-1.py
+++ project-1.py
@@ -2,6 +2,7 @@
 #total rent
 # total food ordered for snacking
 # charge per unit
+#Persons living in room or flat
 
 #output
 #total amount you've to pay is
@@ -10,3 +11,8 @@ rent = int(input("Enter the flat rent:"))
 food = int (input("enter the total food ordered ="))
 electricity_spent = int(input("enter the total of electricity spend:"))
 charge_per_unit = int(input("enter the the charge per unit:"))
+persons = int(input("enter the number of persons living in room or flat:"))
+
+total_electricity_bill = electricity_spent*charge_per_unit
+output = (rent+food+total_electricity_bill)// persons
+print("each person will pay:",output)
