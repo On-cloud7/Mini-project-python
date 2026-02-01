e5b18f533d43ab6f24de49f1a9cb4278fdf3235e
On-cloud7 <priyanka.p62003@gmail.com>
2026-02-01 21:07:38 +0530

    changes made

--- project-2.py
+++ project-2.py
@@ -1,1 +1,38 @@
 #rock-paper scissor game:
+"""_summary_
+1- input from user (rock,paper,scissor)
+2- computer choice (computer will choose randomly not conditionally)
+3- Result print
+
+cases:
+A - Rock
+Rock - Rock = tie
+Rock -paper = Paper wins
+Rock -scissor = Rock wins
+
+B - Paper
+paper- paper = tie
+paper - rock = paper win
+paper - scissor = scissor win
+
+C - Scissor
+Scissor - Scissor = win
+Scissor - Rock = Rock win
+Scissor - paper = Scissor wins
+
+    """
+import random
+item_list = ["rock","paper","Scissor"]
+user_choice = input("enter your move = rock,paper,scissor:")
+comp_choice = random.choice(item_list)
+print(f"user choice = {user_choice}, computer choice = {comp_choice}")
+
+if user_choice == comp_choice:
+    print("both chooses same :  = Match tie")
+elif user_choice == "rock":
+    if comp_choice == "paper":
+        print("paper wins : computer wins")
+    else:
+        print("rock wins : user wins")
+        
+
